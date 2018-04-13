d075325d3d0976fd8f406138473edb01216f0508
Michel Perez <michel.ingesoft@gmail.com>
2018-04-12 21:18:30 -0500

    wip

---
+++ src/crystalapp.cr
@@ -0,0 +1,6 @@
+require "./crystalapp/*"
+
+# TODO: Write documentation for `Crystalapp`
+module Crystalapp
+  # TODO: Put your code here
+end
---
+++ src/crystalapp/version.cr
@@ -0,0 +1,3 @@
+module Crystalapp
+  VERSION = "0.1.0"
+end
