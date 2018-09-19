88c37a67b35857f21ec675d5532347174e88e62f
Michel Perez <michel.ingesoft@gmail.com>
2018-09-19 14:50:37 -0500

    wip

--- src/crystalapp.cr
+++ src/crystalapp.cr
@@ -1,15 +1,27 @@
 require "./crystalapp/*"
 
+abstract class Salute
+  abstract def hello : String
+end
+
 module Crystalapp
-  def hello(name : String?)
-    if name # validates nil
-      cad = name + name
-      puts "Hola #{cad}"
+  class Salutator < Salute
+    property :name
+
+    def initialize(@name : String?)
+    end
+
+    def hello
+      if name = @name # validates nil
+        cad = name + name
+        puts "Hola #{cad}"
+      end
     end
   end
 end
 
-include Crystalapp
+def main(mod : Salute)
+  mod.hello
+end
 
-hello("Demo")
-hello(nil)
+main(Crystalapp::Salutator.new("Demo"))
